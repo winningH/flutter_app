a77bbb717aac60c6294c5ed6a65e31770222c918
hwayn <hwayning@gmail.com>
2020-09-16 12:03:23 +0800

    update

--- lib/find/ArticleItem.dart
+++ lib/find/ArticleItem.dart
@@ -15,45 +15,77 @@ class ArticleItem extends StatelessWidget {
       child: Card(
         elevation: 10,
         child: Padding(
-          padding: EdgeInsets.all(5),
+          padding: EdgeInsets.all(10),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               new Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    model.name,
-                    style: TextStyle(fontSize: 18, color: Colors.black),
-                  ),
+                children: [
                   Row(
                     children: <Widget>[
-                      Text(
-                        model.type,
-                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(model.logo),
+                            fit: BoxFit.cover
+                          )
+                        ),
                       ),
                       SizedBox(width: 10),
-                      Icon(Icons.local_florist, size: 20, color: Colors.grey),
                       Text(
-                        model.count,
-                        style: TextStyle(fontSize: 12, color: Colors.grey),
-                      )
-                    ], 
-                  )
+                        model.location.substring(0, model.location.length > 6
+                          ? 6
+                          : model.location.length),
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('| ' + model.type, 
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.grey
+                            )
+                          ),
+                          Text('| ' + model.size, 
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.grey
+                            )
+                          ),
+                          Text('| ' + model.employee,
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.grey
+                            )
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  Divider(height: 20, color: Colors.red),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        '热招：${model.hot} 等${model.count}个职位',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey
+                        ),
+                      ),
+                      Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+                    ],
+                  ),
                 ],
-              ),
-              Divider(color: Colors.red,),
-              Container(
-                width: 80,
-                height: 80,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: NetworkImage(model.logo),
-                    fit: BoxFit.cover
-                  )
-                ),
               ),
             ],
           ),
